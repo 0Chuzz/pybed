17d25bcf1e7b82390cff2182e066f0e8663968a0
0Chuzz <chuzz.chuzz@gmail.com>
2015-12-11 16:47:35 +0100

    more types stuff

--- src/pybed.c
+++ src/pybed.c
@@ -2,96 +2,112 @@
 #include <bed.h>
 
 typedef struct {
-    PyObject_HEAD
-    /* Type-specific fields go here. */
-    bed_node node;
+        PyObject_HEAD
+        /* Type-specific fields go here. */
+        bed_node node;
+        PyObject *lowRef, *hiRef;
 } bed_nodeObject;
 
+static PyObject* Node_new (PyTypeObject * type, PyObject* args, PyObject *kwds)
+{
+        bed_nodeObject *self;
+        self = type->tp_alloc(type, 0);
+        int size;
+        if (!PyArg_ParseTupleAndKeywords(args, kwds, "d", "", &size)) return NULL;
+        //bed_mk();
+        return NULL; //TODO
+
+}
+
 static PyTypeObject bed_nodeType = {
-    PyObject_HEAD_INIT(NULL)
-    0,                         /*ob_size*/
-    "bed.Node",             /*tp_name*/
-    sizeof(bed_nodeObject), /*tp_basicsize*/
-    0,                         /*tp_itemsize*/
-    0,                         /*tp_dealloc*/
-    0,                         /*tp_print*/
-    0,                         /*tp_getattr*/
-    0,                         /*tp_setattr*/
-    0,                         /*tp_compare*/
-    0,                         /*tp_repr*/
-    0,                         /*tp_as_number*/
-    0,                         /*tp_as_sequence*/
-    0,                         /*tp_as_mapping*/
-    0,                         /*tp_hash */
-    0,                         /*tp_call*/
-    0,                         /*tp_str*/
-    0,                         /*tp_getattro*/
-    0,                         /*tp_setattro*/
-    0,                         /*tp_as_buffer*/
-    Py_TPFLAGS_DEFAULT,        /*tp_flags*/
-    "bed_node objects",           /* tp_doc */
+        PyObject_HEAD_INIT(NULL)
+        0,                     /*ob_size*/
+        "bed.Node",         /*tp_name*/
+        sizeof(bed_nodeObject), /*tp_basicsize*/
+        0,                     /*tp_itemsize*/
+        0,                     /*tp_dealloc*/
+        0,                     /*tp_print*/
+        0,                     /*tp_getattr*/
+        0,                     /*tp_setattr*/
+        0,                     /*tp_compare*/
+        0,                     /*tp_repr*/
+        0,                     /*tp_as_number*/
+        0,                     /*tp_as_sequence*/
+        0,                     /*tp_as_mapping*/
+        0,                     /*tp_hash */
+        0,                     /*tp_call*/
+        0,                     /*tp_str*/
+        0,                     /*tp_getattro*/
+        0,                     /*tp_setattro*/
+        0,                     /*tp_as_buffer*/
+        Py_TPFLAGS_DEFAULT,    /*tp_flags*/
+        "bed_node objects",       /* tp_doc */
 };
 
 typedef struct {
-    PyObject_HEAD
-    /* Type-specific fields go here. */
-    bed_var var;
+        PyObject_HEAD
+        /* Type-specific fields go here. */
+        bed_var var;
+        unsigned int size;
 } bed_varObject;
+static PyObject* Var_new (PyTypeObject * type, PyObject* args, PyObject *kwds)
+{
+        bed_varObject *self;
+        int size = 1;
+
+        if (!PyArg_ParseTupleAndKeywords(args, kwds, "", "", &size))
+                return NULL;
+        self = type->tp_alloc(type, 0);
+        self->var = bed_new_variables(size);
+        self->size = size;
+        return self;
+
+}
 
 static PyTypeObject bed_varType = {
-    PyObject_HEAD_INIT(NULL)
-    0,                         /*ob_size*/
-    "bed.Var",             /*tp_name*/
-    sizeof(bed_varObject), /*tp_basicsize*/
-    0,                         /*tp_itemsize*/
-    0,                         /*tp_dealloc*/
-    0,                         /*tp_print*/
-    0,                         /*tp_getattr*/
-    0,                         /*tp_setattr*/
-    0,                         /*tp_compare*/
-    0,                         /*tp_repr*/
-    0,                         /*tp_as_number*/
-    0,                         /*tp_as_sequence*/
-    0,                         /*tp_as_mapping*/
-    0,                         /*tp_hash */
-    0,                         /*tp_call*/
-    0,                         /*tp_str*/
-    0,                         /*tp_getattro*/
-    0,                         /*tp_setattro*/
-    0,                         /*tp_as_buffer*/
-    Py_TPFLAGS_DEFAULT,        /*tp_flags*/
-    "bed_var objects",           /* tp_doc */
+        PyObject_HEAD_INIT(NULL)
+        .tp_name = "bed.Var",         /*tp_name*/
+        .tp_basicsize = sizeof(bed_varObject), /*tp_basicsize*/
+        .tp_doc = "bed_var object",
+        .tp_new = Node_new,
 };
 
 
 
 
 static PyObject *
-pybed_init(PyObject *self, PyObject *args)
+pybed_not(PyObject *self, PyObject *args)
 {
-    unsigned long n, c;
-    int sts;
+        bed_nodeObject *base,*ret;
+        int sts;
 
 
-    if (!PyArg_ParseTuple(args, "uu", &n, &c))
-        return NULL;
-    return Py_None;
+        if (!PyArg_ParseTuple(args, "O!", &bed_varType, &base))
+                return NULL;
+
+        ret = bed_nodeType.tp_new(&bed_nodeType, Py_None, Py_None);
+        if (ret == NULL) return NULL;
+        ret->node = bed_mk_not(base->node);
+        ret->lowRef = base;
+        ret->hiRef = NULL;
+        Py_INCREF(base);
+        return ret;
 }
 static PyMethodDef pyBedMethods[] = {
 
-    {"init",  pybed_init, METH_VARARGS,
-     "bed_init"},
-    {NULL, NULL, 0, NULL}        /* Sentinel */
+        {"not",  pybed_not, METH_VARARGS,
+         "not"},
+        {NULL, NULL, 0, NULL}    /* Sentinel */
 };
 
 PyMODINIT_FUNC
 initbed(void)
 {
-    bed_init(10*1024*1024,5*1024*1024);
+        bed_init(10*1024*1024,5*1024*1024);
 
-    PyObject *module = Py_InitModule("bed", pyBedMethods);
-    Py_INCREF(bed_nodeType);
-    PyModule_AddObject(module, "Node", bed_nodeType);
-    Py_INCREF(bed_varType);
-    PyModule_AddObject(module, "Var", bed_varType);
+        PyObject *module = Py_InitModule("bed", pyBedMethods);
+        Py_INCREF(&bed_nodeType);
+        PyModule_AddObject(module, "Node", &bed_nodeType);
+        Py_INCREF(&bed_varType);
+        PyModule_AddObject(module, "Var", &bed_varType);
 }
