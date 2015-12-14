72afcd7f89da991a7860ae5c4dbd55f03676daa5
0Chuzz <chuzz.chuzz@gmail.com>
2015-12-14 12:05:03 +0100

    added small test, fixed initialization

--- src/pybed.c
+++ src/pybed.c
@@ -8,15 +8,11 @@ typedef struct {
     PyObject *lowRef, *hiRef;
 } bed_nodeObject;
 
-static PyObject* Node_new (PyTypeObject * type, PyObject* args, PyObject *kwds)
-{
-    bed_nodeObject *self;
-    self = type->tp_alloc(type, 0);
-    int size;
-    if (!PyArg_ParseTupleAndKeywords(args, kwds, "d", "", &size)) return NULL;
-    //bed_mk();
-    return NULL;         //TODO
 
+static void Node_dealloc(bed_nodeObject *self)
+{
+    Py_XDECREF(self->lowRef);
+    Py_XDECREF(self->hiRef);
 }
 
 static PyTypeObject bed_nodeType = {
@@ -25,7 +21,8 @@ static PyTypeObject bed_nodeType = {
     .tp_basicsize = sizeof(bed_nodeObject),         /*tp_basicsize*/
     .tp_flags = Py_TPFLAGS_DEFAULT,            /*tp_flags*/
     .tp_doc = "bed_node objects",               /* tp_doc */
-    .tp_new = Node_new
+    .tp_new = PyType_GenericNew,
+    .tp_alloc = PyType_GenericAlloc
 };
 
 typedef struct {
@@ -39,8 +36,6 @@ static PyObject* Var_new (PyTypeObject * type, PyObject* args, PyObject *kwds)
     bed_varObject *self;
     int size = 1;
 
-    if (!PyArg_ParseTupleAndKeywords(args, kwds, "", "", &size))
-        return NULL;
     self = type->tp_alloc(type, 0);
     self->var = bed_new_variables(size);
     self->size = size;
@@ -54,6 +49,7 @@ static PyTypeObject bed_varType = {
     .tp_basicsize = sizeof(bed_varObject),         /*tp_basicsize*/
     .tp_doc = "bed_var object",
     .tp_new = Var_new,
+    .tp_alloc = PyType_GenericAlloc
 };
 
 
@@ -63,7 +59,7 @@ pybed_not(PyObject *self, PyObject *args)
     bed_nodeObject *base,*ret;
     int sts;
 
-    if (!PyArg_ParseTuple(args, "O!", &bed_varType, &base))
+    if (!PyArg_ParseTuple(args, "O!", &bed_nodeType, &base))
         return NULL;
 
     ret = bed_nodeType.tp_new(&bed_nodeType, Py_None, Py_None);
@@ -75,11 +71,31 @@ pybed_not(PyObject *self, PyObject *args)
     return ret;
 }
 
+static PyObject *
+pybed_var(PyObject *self, PyObject *args)
+{
+    bed_nodeObject *ret;
+    bed_varObject *base;
+    int sts;
+
+    if (!PyArg_ParseTuple(args, "O!", &bed_varType, &base))
+        return NULL;
+
+    ret = bed_nodeType.tp_new(&bed_nodeType, Py_None, Py_None);
+    if (ret == NULL) return NULL;
+    ret->node = bed_mk_var(base->var, bed_false, bed_true);
+    ret->lowRef = (PyObject *) base;
+    ret->hiRef = NULL;
+    Py_INCREF(base);
+    return ret;
+}
 
 static PyMethodDef pyBedMethods[] = {
 
-    {"not",  pybed_not, METH_VARARGS,
-     "not"},
+    {"not_",  pybed_not, METH_VARARGS,
+     "not node"},
+    {"var",  pybed_var, METH_VARARGS,
+      "var node"},
     {NULL, NULL, 0, NULL}            /* Sentinel */
 };
 
---
+++ tests/bed_module.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python2
+import bed
+print bed.not_(bed.var(bed.Var()))
